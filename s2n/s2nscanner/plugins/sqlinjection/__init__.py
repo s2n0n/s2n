c282de6213aea1e241e01ca1e9c03b490ef1c4f8
DDONGHUNI <mi1232944@naver.com>
2025-11-11 15:29:19 +0900

    fix: __init__.py 파일 원복

--- s2n/s2nscanner/plugins/sqlinjection/__init__.py
+++ s2n/s2nscanner/plugins/sqlinjection/__init__.py
@@ -1,5 +1,3 @@
-# 플러그인 표준에 따라 __init__.py 파일을 생성합니다.
-# sqli_main.py 파일 내의 SQLInjectionPlugin 클래스를 임포트합니다.
-from .sqli_main import SQLInjectionPlugin
+from .sqli_main import main as Plugin
 
-__all__ = ["SQLInjectionPlugin"]
+__all__ = ["Plugin"]
